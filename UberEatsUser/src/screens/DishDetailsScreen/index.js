79f37076663232a8a4f7afb2d52cde0ef6701315
Jordan Devaney <jordandevaney28@gmail.com>
2022-07-17 22:47:28 -0400

    menu item screen for user app

---
+++ UberEatsUser/src/screens/DishDetailsScreen/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import restaurants from "../../../assets/data/restaurants.json";
+import { AntDesign } from "@expo/vector-icons";
+
+const dish = restaurants[0].dishes[0];
+
+const DishDetailsScreen = () => {
+	const [quantity, setQuantity] = React.useState(1);
+
+	const onMinus = () => {
+		if (quantity > 1) {
+			setQuantity(quantity - 1);
+		}
+	};
+
+	const onPlus = () => {
+		setQuantity(quantity + 1);
+	};
+	//  calculate total price
+	const totalPrice = quantity * dish.price;
+	return (
+		<View style={styles.page}>
+			<Text style={styles.title}>{dish.name}</Text>
+			<Text style={styles.description}>{dish.description}</Text>
+			<View style={styles.seperator} />
+			<View style={styles.row}>
+				<AntDesign
+					name="minuscircleo"
+					size={60}
+					color={"black"}
+					onPress={onMinus}
+				/>
+				<Text style={styles.quantity}>{quantity}</Text>
+				<AntDesign
+					name="pluscircleo"
+					size={60}
+					color={"black"}
+					onPress={onPlus}
+				/>
+			</View>
+			<View style={styles.button}>
+				<Text style={styles.buttonText}>
+					Add {quantity} to basket &#8226; ${totalPrice.toFixed(2)}
+				</Text>
+			</View>
+		</View>
+	);
+};
+const styles = StyleSheet.create({
+	page: {
+		flex: 1,
+		width: "100%",
+		paddingVertical: 30,
+	},
+	title: {
+		fontSize: 30,
+		fontWeight: "600",
+		marginVertical: 10,
+	},
+	seperator: {
+		height: 1,
+		backgroundColor: "lightgrey",
+		marginVertical: 10,
+	},
+	description: {
+		color: "grey",
+	},
+	row: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 50,
+	},
+	quantity: {
+		fontSize: 25,
+		marginHorizontal: 20,
+	},
+	button: {
+		backgroundColor: "black",
+		padding: 20,
+		marginTop: "auto",
+		alignItems: "center",
+	},
+	buttonText: {
+		color: "white",
+		fontSize: 20,
+		fontWeight: "600",
+	},
+});
+
+export default DishDetailsScreen;
